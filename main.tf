c88c00c78f5acf7426071aca190b2ea20591f936
Erik <128736440+NewName4Me@users.noreply.github.com>
2025-01-05 17:38:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,29 @@
-provider "aws" {
-    region = "us-east-1"  
-}
-
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.31.0"
+    }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Application = "Web EC2 S3"
+      Owner       = "Erik Tortajada"
+    }
+  }
+}
+
+  resource "aws_vpc" "vpc" {
+    cidr_block           = "10.0.0.0/16"
+    instance_tenancy     = "default"
+    enable_dns_hostnames = true
+
+    tags = {
+      Name = "custom_vpc"
+    }
+  }
